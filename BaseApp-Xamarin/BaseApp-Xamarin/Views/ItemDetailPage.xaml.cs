36ebcd29c842b9026c2c96d0d864b28eda35f75b
femi-oz <femiosibodu@gmail.com>
2022-06-29 17:09:34 +0100

    First commit

---
+++ BaseApp-Xamarin/BaseApp-Xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using BaseApp_Xamarin.ViewModels;
+
+namespace BaseApp_Xamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ BaseApp-Xamarin/BaseApp-Xamarin/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using BaseApp_Xamarin.Models;
+using BaseApp_Xamarin.ViewModels;
+
+namespace BaseApp_Xamarin.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
